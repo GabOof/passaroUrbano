e40f3035b028943e88b1d6134208941ed921c4cc
Gabrielle Fonseca <gabrielle.fonseca@zenklub.com>
2023-11-14 18:23:15 -0300

    installing necessary libs

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import '../node_modules/jquery/dist/jquery.min.js';
+import '../node_modules/tether/dist/js/tether.min.js';
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
