db6c3e09c81efe3a5c1f3bd4bbe9dc14411f2f8d
Gabrielle Fonseca <gabrielle.fonseca@zenklub.com>
2023-11-16 10:54:52 -0300

    creating page components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { TopoComponent } from './topo/topo.component';
+import { HomeComponent } from './home/home.component';
+import { RodapeComponent } from './rodape/rodape.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, TopoComponent, HomeComponent, RodapeComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-topo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './topo.component.html',
+  styleUrl: './topo.component.css'
+})
+export class TopoComponent {
+
+}
